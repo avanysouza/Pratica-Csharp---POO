cb264b6fb0ef00c5fcc1061243d25b44504c7dcf
avanysouza <avanysouza@gmail.com>
2024-01-12 16:04:47 +0000

    Adicionar arquivos de projeto.

---
+++ IMC/Pessoa.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praticas.IMC
+{
+    internal class Pessoa
+    {
+
+        public double peso;
+
+        public double altura;
+
+        public double calculoImc()
+        {
+            return peso / (altura * altura);
+        }
+
+        public string situacao(double imc)
+        {
+            string retorno;
+
+            if (imc < 18.5)
+            {
+                retorno = "Abaixo do peso";
+            }
+            else if (imc < 25)
+            {
+                retorno = "Peso normal";
+            }
+            else if (imc < 30)
+            {
+                retorno = "Acima do peso";
+            }
+            else if (imc < 35)
+            {
+                retorno = "Obesidade I";
+
+            }
+            else if (imc < 40)
+            {
+                retorno = "Obesidade II";
+            }
+            else
+            {
+                retorno = "Obesidade III";
+            }
+
+            return retorno;
+        }
+
+        public void mensagem()
+        {
+            double obterCalculo = calculoImc();
+
+            string obterSituacao = situacao(obterCalculo);
+
+            Console.WriteLine("Seu IMC é de " + obterCalculo);
+            Console.WriteLine("Sua situação é " + obterSituacao);
+        }
+
+    }
+}
---
+++ IMC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praticas.IMC
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Pessoa a = new Pessoa();
+
+            Console.WriteLine("Informe seu peso:");
+            a.peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informa sua altura:");
+            a.altura = Convert.ToDouble(Console.ReadLine());
+
+            a.mensagem();
+
+
+        }
+
+
+
+
+    }
+}
